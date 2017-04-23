0ac1fb991b652a3b8bb063160cd23eeaca33a449
Michael Obi <ask4myk@gmail.com>
2017-04-23 12:14:44 +0100

    Save User profile in firebase

--- app/src/main/java/com/example/lyndaob/reminderapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/lyndaob/reminderapplication/RegistrationActivity.java
@@ -4,6 +4,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -15,10 +18,12 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.lyndaob.reminderapplication.models.User;
+
 public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "RegistrationActivity";
-    TextView tvEmail, tvPassword, tvConfirmPassword;
+    TextView tvEmail, tvPassword, tvConfirmPassword, tvName, tvPhone;
     View registrationForm;
     ProgressBar progressBar;
     private FirebaseAuth mAuth;
@@ -37,7 +42,8 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
         tvEmail = (TextView) findViewById(R.id.email);
         tvPassword = (TextView) findViewById(R.id.password);
         tvConfirmPassword = (TextView) findViewById(R.id.confirm_password);
-
+        tvPhone = (TextView) findViewById(R.id.phone);
+        tvName = (TextView) findViewById(R.id.name);
     }
 
     @Override
@@ -59,7 +65,7 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
         }
     }
 
-    private void createUserWithEmail(String email, String password) {
+    private void createUserWithEmail(final String email, String password) {
         Toast.makeText(this, "Registering User...", Toast.LENGTH_LONG).show();
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -74,12 +80,22 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
                             registrationForm.setVisibility(View.VISIBLE);
                             progressBar.setVisibility(View.GONE);
                             Log.w(TAG, "RegisterWithEmail:failed", task.getException());
-                            Toast.makeText(RegistrationActivity.this, task.getException().getMessage(),
-                                    Toast.LENGTH_SHORT).show();
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthUserCollisionException e) {
+                                Log.d(TAG, "onComplete: " + e.getMessage(), e);
+                                Toast.makeText(RegistrationActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                             return;
                         }
                         Log.d(TAG, "onComplete: Registration successful");
-                        //TODO: Save the User's registration details
+                        String name = tvName.getText().toString();
+                        String phoneNumber = tvPhone.getText().toString();
+                        User user = new User(name, email, phoneNumber);
+                        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+                        database.child("users").child(task.getResult().getUser().getUid()).setValue(user);
                         Toast.makeText(RegistrationActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
                     }
                 });
